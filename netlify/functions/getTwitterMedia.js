7aeffb7639647da1ca1f565c911e33878890d04d
Sayfullah Sayeb <sayfullah.sayeb@gmail.com>
2025-01-30 01:22:45 +0600

    Fixed node-fetch issue

--- netlify/functions/getTwitterMedia.js
+++ netlify/functions/getTwitterMedia.js
@@ -10,16 +10,19 @@ exports.handler = async function(event) {
     try {
         const response = await fetch(url, { headers: { "User-Agent": "Mozilla/5.0" } });
         const html = await response.text();
-        const $ = cheerio.load(html);
 
+        console.log("Fetched HTML:", html); // DEBUG: Print Twitter's response
+
+        const $ = cheerio.load(html);
         let mediaUrls = [];
+
         $("meta[property='og:image'], meta[property='og:video']").each((_, element) => {
             mediaUrls.push($(element).attr("content"));
         });
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ media: mediaUrls }),
+            body: JSON.stringify({ media: mediaUrls, debug: html.substring(0, 1000) }), // Send partial HTML for debugging
         };
     } catch (error) {
         return {
